cfaaa3c11b9d7c4b0fdaa356494c8309bbfa39f2
Brendon Gustavo Feitosa <Brendon Gustavo Feitosa>
2025-10-09 20:10:36 -0300

    Finalizando exercicio 1

--- app.py
+++ app.py
@@ -2,6 +2,12 @@ from flask import Flask, request, render_template
 
 app = Flask(__name__) #objeto que vai rodar na função main do python
 
+alunos = [
+    {"id": 1, "nome": "Brendon"},
+    {"id": 2, "nome": "Debora"},
+    {"id": 3, "nome": "Helena"}
+]
+
 @app.route('/')
 def home():
     return render_template('index.html')
@@ -17,6 +23,40 @@ def cadastrar():
     user_id = request.args.get('id')
     nome = request.args.get('nome')
     return f"Usuário cadastrado: id: {user_id} - Nome: {nome}"
+
+#endpoint buscar todos os alunos
+@app.route('/listar', methods=['GET'])
+def listar():
+    return alunos
+
+
+#buscar aluno
+@app.route('/buscaraluno/<int:id>', methods=['GET'])
+def buscar_aluno(id):
+    for aluno in alunos:
+        if aluno["id"] == id:
+            return aluno
+    
+    return "Aluno não localizado"
+
+#remover aluno
+@app.route('/remover/<int:id>', methods=['DELETE'])
+def remover_aluno(id):
+    for aluno in alunos:
+        if aluno["id"] == id:
+            alunos.remove(aluno)   # remove o dicionário da lista
+            return "Aluno removido com sucesso!"
+
+    return "Aluno não localizado"
+
+
+#parametros na rota como QueryString
+@app.route('/cadastrarjson', methods=['POST'])
+def cadastrar_json():
+    novo = request.get_json()
+    novo["id"] = len(alunos) + 1
+    alunos.append(novo)
+    return alunos
 
 
 if __name__ == '__main__': #checando se main existe no __name__
